3c2f261c47f97d8fe5f0e6106e50a930258ab029
Marcos Trejo Reyes <marcostrejoreyes@gmail.com>
2019-02-08 11:32:37 -0600

    Componente graficos y practica de input y output

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,16 +6,23 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule} from '@angular/forms';
 
 // Routes
 import { PAGES_ROUTES } from './pages.routes';
+// tmp
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficaDonaComponent } from '../components/grafica-dona/grafica-dona.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
-    PagesComponent
+    PagesComponent,
+    IncrementadorComponent,
+    GraficaDonaComponent
   ],
   exports: [
     DashboardComponent,
@@ -25,7 +32,9 @@ import { PAGES_ROUTES } from './pages.routes';
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ]
 
 })
